ac23ec6266a322471d917feaaea6fc290046310b
Lynette <lynetteacholah@icloud.com>
2020-09-21 12:22:06 +0300

    the delete button works

--- notes-functions.js
+++ notes-functions.js
@@ -14,6 +14,42 @@ const saveNotes = function(notes) {
   localStorage.setItem("notes", JSON.stringify(notes))
 };
 
+//remove an item from the list
+const removeNote = function(id) {
+  const notesIndex = notes.findIndex(function(note){
+    return note.id === id
+  })
+  if(notesIndex > -1) {
+    notes.splice(notesIndex, 1)
+  }
+}
+
+//render the DOm structure for a note
+const generateNoteDOM = function(note) {
+  var noteEl = document.createElement("div");
+  var textEl = document.createElement("span")
+  const button = document.createElement("button")
+
+  //set up content for delte button
+  button.textContent = "delete"
+  noteEl.appendChild(button);
+
+  button.addEventListener("click", function() {
+    removeNote(note.id)
+    saveNotes(notes)
+    renderNotes(notes, filters)
+  })
+
+
+  if (note.title.length > 0) {
+    noteEl.textContent = note.title;
+  }else{
+    textEl.textContent = "Lynette";
+  }
+  noteEl.appendChild(textEl);
+  return noteEl
+}
+
 var renderNotes = function(notesData, filtersData){
   var filteredNotes = notesData.filter(function (note) {
     return note.title.toLowerCase().includes(filtersData.searchText.toLowerCase())
@@ -21,13 +57,15 @@ var renderNotes = function(notesData, filtersData){
     document.querySelector("#notes").innerHTML = ""
 
     filteredNotes.forEach(function(note){
-      var noteEl = document.createElement("p")
+      const noteEl = generateNoteDOM(note)
+      //DOM structure
+    //   var noteEl = document.createElement("p")
 
-      if (note.title.length > 0) {
-        noteEl.textContent = note.title;
-    }else{
-        noteEl.textContent = "unnamed note";
-    }
+    //   if (note.title.length > 0) {
+    //     noteEl.textContent = note.title;
+    // }else{
+    //     noteEl.textContent = "unnamed note";
+    // }
       document.querySelector("#notes").appendChild(noteEl)
   })
 };
